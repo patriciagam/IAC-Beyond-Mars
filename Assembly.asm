4472ea9b1a65eed63c849c84976f0f18da8600d5
margarida <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-05-20 16:06:24 +0100

    onde podemos fazer o codigo

---
+++ Assembly.asm
@@ -0,0 +1,8 @@
+; *********************************************************************
+; * IST
+; * Nome: projeto Assembly
+; * Descrição: Space invaders
+; * Autores: Ines Almeida Paredes 107028
+; *          Patricia Antunes Gameiro 107245
+; *          Margarida Ascensao Lourenco 107137
+; *********************************************************************
